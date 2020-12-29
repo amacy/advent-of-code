533b2f54bbbd281534f89ff7bfb68daaa17a2ca8
amacy <aaronmacy@gmail.com>
2020-12-29 15:33:58 -0500

    day 5, part 1: solved. part 2: works for sample input only

--- lib/2015/day_05.rb
+++ lib/2015/day_05.rb
@@ -4,15 +4,65 @@ class Day05
   end
 
   def part_1
-    # solve part 1
+    @input.select do |string|
+      %w(a e i o u).map { |vowel| string.count(vowel) }.sum >= 3 &&
+        string.match(/(ab|cd|pq|xy)/).nil? &&
+        !_contains_consecutive_values?(string)
+    end.count
   end
 
   def part_2
-    # solve part 2
+    @input.select do |string|
+      array = string.split("")
+      _contains_repeat_separated_by_one_letter?(array) &&
+        _contains_repeating_pair?(array)
+    end.count
+  end
+
+  def _contains_repeating_pair?(array)
+    index = 1
+
+    loop do
+      return false if array[index + 1..-1].empty?
+
+      candidate = array[index - 1..index]
+      array[index + 1..-1].each_slice(2) do |s|
+        return true if candidate == s
+      end
+
+      array[index + 2..-1].each_slice(2) do |s|
+        return true if candidate == s
+      end
+
+      index += 1
+    end
+  end
+
+  def _contains_repeat_separated_by_one_letter?(array)
+    array.each_slice(3) do |s|
+      return true if s[0] == s[2]
+    end
+
+    array[1..-1].each_slice(3) do |s|
+      return true if s[0] == s[2]
+    end
+    false
+  end
+
+  def _contains_consecutive_values?(string)
+    array = string.split("")
+    array.each_slice(2) do |s|
+      return false if s[0] == s[1]
+    end
+
+    array[1..-1].each_slice(2) do |s|
+      return false if s[0] == s[1]
+    end
+    true
   end
 
   def _parse_input(input)
-    input ||= File.read("config/day_05.txt")
-    # do something with the input
+    input ||= File.read("config/2015/day_05.txt")
+    input.split("\n")
   end
 end
--- spec/2015/day_05_spec.rb
+++ spec/2015/day_05_spec.rb
@@ -4,22 +4,44 @@ require_relative "../../lib/2015/day_05.rb"
 RSpec.describe Day05 do
   context "part 1" do
     it "works for the sample input" do
-      input = ""
-      expect(Day05.new(input).part_1).to eq 0
+      input_1 = "ugknbfddgicrmopn"
+      expect(Day05.new(input_1).part_1).to eq 1
+
+      input_2 = "aaa"
+      expect(Day05.new(input_2).part_1).to eq 1
+
+      input_3 = "jchzalrnumimnmhp"
+      expect(Day05.new(input_3).part_1).to eq 0
+
+      input_4 = "haegwjzuvuyypxyu"
+      expect(Day05.new(input_4).part_1).to eq 0
+
+      input_5 = "dvszwmarrgswjxmb"
+      expect(Day05.new(input_5).part_1).to eq 0
     end
 
     it "works for the puzzle input" do
-      expect(Day05.new.part_1).to eq 0
+      expect(Day05.new.part_1).to eq 238
     end
   end
 
   context "part 2" do
     it "works for the sample input" do
-      input = ""
-      expect(Day05.new(input).part_2).to eq 0
+      input_1 = "qjhvhtzxzqqjkmpb"
+      expect(Day05.new(input_1).part_2).to eq 1
+
+      input_2 = "xxyxx"
+      expect(Day05.new(input_2).part_2).to eq 1
+
+      input_3 = "uurcxstgmygtbstg"
+      expect(Day05.new(input_3).part_2).to eq 0
+
+      input_4 = "ieodomkazucvgmuy"
+      expect(Day05.new(input_4).part_2).to eq 0
     end
 
     it "works for the puzzle input" do
+      # 53 is incorrect
       expect(Day05.new.part_2).to eq 0
     end
   end
