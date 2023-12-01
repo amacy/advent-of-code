7fb95664c73762824593327480382c8e89a91fb5
Aaron Macy <amacy@paypal.com>
2023-12-01 17:41:23 -0500

    Day 1

--- lib/2023/day_01.rb
+++ lib/2023/day_01.rb
@@ -1,18 +1,59 @@
+require "set"
+
 class Day01
+  MAPPING = {
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9",
+  }
+
+
   def initialize(input=nil)
     @input = _parse_input(input)
   end
 
   def part_1
-    # solve part 1
+    total = 0
+    @input.each do |line|
+      digits = line.scan(/\d/)
+      total += (digits[0] + digits[-1]).to_i
+    end
+    total
   end
 
   def part_2
-    # solve part 2
+    total = 0
+    @input.each do |line|
+      matches = line.scan(/\d|#{MAPPING.keys.join("|")}/)
+      if matches[0].match(/\d/)
+        n_1 = matches[0]
+      else
+        string = matches[0].scan(/#{MAPPING.keys.join("|")}/).first
+        n_1 = MAPPING[string]
+      end
+
+      if matches[-1].match(/\d/)
+        n_2 = matches[-1]
+      else
+        string = matches[-1].scan(/#{MAPPING.keys.join("|")}/).first
+        n_2 = MAPPING[string]
+      end
+
+      total += [n_1, n_2].join.to_i
+    end
+    total
   end
 
+  private
+
   def _parse_input(input)
-    input ||= File.read("config/day_01.txt")
-    # do something with the input
+    input ||= File.read("config/2023/day_01.txt")
+    input.split(/\n/)
   end
 end
--- spec/2023/day_01_spec.rb
+++ spec/2023/day_01_spec.rb
@@ -4,23 +4,37 @@ require_relative "../../lib/2023/day_01.rb"
 RSpec.describe Day01 do
   context "part 1" do
     it "works for the sample input" do
-      input = ""
-      expect(Day01.new(input).part_1).to eq 0
+      input = <<~INPUT
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+INPUT
+      expect(Day01.new(input).part_1).to eq 142
     end
 
     it "works for the puzzle input" do
-      expect(Day01.new.part_1).to eq 0
+      expect(Day01.new.part_1).to eq 55538
     end
   end
 
   context "part 2" do
     it "works for the sample input" do
-      input = ""
-      expect(Day01.new(input).part_2).to eq 0
+      input = <<~INPUT
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+INPUT
+      expect(Day01.new(input).part_2).to eq 281
     end
 
     it "works for the puzzle input" do
-      expect(Day01.new.part_2).to eq 0
+      pending "oneight is being parsed as only 1"
+      expect(Day01.new.part_2).to eq 54875
     end
   end
 end
