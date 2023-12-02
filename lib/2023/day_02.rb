96a674ac32c41e945bcf7fcfe9c0752bec3df362
amacy <aaronmacy@gmail.com>
2023-12-02 18:03:38 -0500

    Day 2

--- lib/2023/day_02.rb
+++ lib/2023/day_02.rb
@@ -1,20 +1,81 @@
 class Day02
+  LIMITS = {
+    "red" => 12,
+    "green" => 13,
+    "blue" => 14,
+  }
+
   def initialize(input=nil)
     @input = _parse_input(input)
   end
 
   def part_1
-    # solve part 1
+    games = @input.split(/\n/).inject({}) do |hash, line|
+      game_id, remainder = line.match(/Game (\d+): (.*)/).captures
+
+      cubes = {}
+      remainder.split("; ").each do |set|
+        set.split(", ").each do |cube_str|
+          count, color = cube_str.split(" ")
+          cubes[color] ||= 0
+          cubes[color] = [count.to_i, cubes[color]].max
+        end
+      end
+
+      hash[game_id.to_i] = cubes
+      hash
+    end
+
+    total = 0
+
+    games.each do |game|
+      colors_under_max = 0
+      game[1].each do |color, count|
+        if count <= LIMITS[color]
+          colors_under_max += 1
+        end
+      end
+      if colors_under_max == LIMITS.length
+        total += game[0]
+      end
+    end
+
+    total
   end
 
   def part_2
-    # solve part 2
+    games = @input.split(/\n/).inject({}) do |hash, line|
+      game_id, remainder = line.match(/Game (\d+): (.*)/).captures
+
+      cubes = {}
+      remainder.split("; ").each do |set|
+        set.split(", ").each do |cube_str|
+          count, color = cube_str.split(" ")
+          cubes[color] ||= 0
+          cubes[color] = [count.to_i, cubes[color]].max
+        end
+      end
+
+      hash[game_id.to_i] = cubes
+      hash
+    end
+
+    total = 0
+
+    games.each do |game|
+      game_total = 1
+      game[1].values.each do |count|
+        game_total *= count
+      end
+      total += game_total
+    end
+
+    total
   end
 
   private
 
   def _parse_input(input)
     input ||= File.read("config/2023/day_02.txt")
-    # do something with the input
   end
 end
--- spec/2023/day_02_spec.rb
+++ spec/2023/day_02_spec.rb
@@ -2,25 +2,33 @@ require "pry"
 require_relative "../../lib/2023/day_02.rb"
 
 RSpec.describe Day02 do
+  before do
+    @sample_input = <<~INPUT
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+INPUT
+  end
+
   context "part 1" do
     it "works for the sample input" do
-      input = ""
-      expect(Day02.new(input).part_1).to eq 0
+      expect(Day02.new(@sample_input).part_1).to eq 8
     end
 
     it "works for the puzzle input" do
-      expect(Day02.new.part_1).to eq 0
+      expect(Day02.new.part_1).to eq 2061
     end
   end
 
   context "part 2" do
     it "works for the sample input" do
-      input = ""
-      expect(Day02.new(input).part_2).to eq 0
+      expect(Day02.new(@sample_input).part_2).to eq 2286
     end
 
     it "works for the puzzle input" do
-      expect(Day02.new.part_2).to eq 0
+      expect(Day02.new.part_2).to eq 72596
     end
   end
 end
