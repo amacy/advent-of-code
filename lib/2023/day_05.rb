de11ab014e30d442c755a9b3ca34383ddd59ea38
amacy <aaronmacy@gmail.com>
2023-12-06 08:39:16 -0500

    day 5 part 1: works for sample input, but needs optimization for full input

--- lib/2023/day_05.rb
+++ lib/2023/day_05.rb
@@ -4,7 +4,32 @@ class Day05
   end
 
   def part_1
-    # solve part 1
+    map_queue, mappings = @input
+
+    results = {}
+
+    mappings.each_with_index do |(current_destination, values), index|
+      if current_destination.nil?
+        break
+      elsif results.empty?
+        values.each do |val|
+          results[val] = {current_destination => val}
+        end
+      else
+        current_source = map_queue[index - 1]
+
+        results.each do |seed, values|
+          source_val = values[current_source]
+          destination_val = mappings[current_destination][source_val] ||
+            source_val
+          results[seed][current_destination] = destination_val
+        end
+      end
+    end
+
+    results.values.map do |values|
+      values[map_queue.last]
+    end.min
   end
 
   def part_2
@@ -15,6 +40,29 @@ class Day05
 
   def _parse_input(input)
     input ||= File.read("config/2023/day_05.txt")
-    # do something with the input
+    lines = input.split(/\n/).map(&:strip)
+
+    mappings = {"seed" => lines[0].scan(/\d+/).map(&:to_i)}
+    current_source = "seed"
+    map_queue = [current_source]
+
+    lines[2..-1].each do |line|
+      if match = line.match(/^#{current_source}-to-(\w+) map:$/)
+        current_source = match.captures.first
+        mappings[current_source] = {}
+        map_queue << current_source
+      elsif line.scan(/\d+/).map(&:to_i).length > 0
+        destination_start, source_start, range_length = line.scan(/\d+/).map(&:to_i)
+
+        source_ns = source_start..source_start + range_length
+        destination_ns = (destination_start..destination_start + range_length).to_a
+
+        source_ns.each_with_index do |source_n, index|
+          mappings[current_source][source_n] = destination_ns[index]
+        end
+      end
+    end
+
+    [map_queue, mappings]
   end
 end
--- spec/2023/day_05_spec.rb
+++ spec/2023/day_05_spec.rb
@@ -2,10 +2,47 @@ require "pry"
 require_relative "../../lib/2023/day_05.rb"
 
 RSpec.describe Day05 do
+  before do
+    @sample_input = <<~INPUT
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+INPUT
+  end
+
   context "part 1" do
     it "works for the sample input" do
-      input = ""
-      expect(Day05.new(input).part_1).to eq 0
+      expect(Day05.new(@sample_input).part_1).to eq 35
     end
 
     it "works for the puzzle input" do
@@ -15,8 +52,7 @@ RSpec.describe Day05 do
 
   context "part 2" do
     it "works for the sample input" do
-      input = ""
-      expect(Day05.new(input).part_2).to eq 0
+      expect(Day05.new(@sample_input).part_2).to eq 0
     end
 
     it "works for the puzzle input" do
