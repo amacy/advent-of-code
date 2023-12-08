238974e8d5d7261ae2e676dda2e5e2f71f399dc4
amacy <aaronmacy@gmail.com>
2023-12-08 07:37:51 -0500

    day 6, part 1

--- lib/2023/day_06.rb
+++ lib/2023/day_06.rb
@@ -4,7 +4,26 @@ class Day06
   end
 
   def part_1
-    # solve part 1
+    times, distances = @input
+
+    times_to_distance = {}
+    times.each_with_index do |time, index|
+      times_to_distance[time] = distances[index]
+    end
+
+    times_to_winners = {}
+    (1..times.max - 1).each do |hold_time|
+      times_to_distance.each do |race_time, distance_record|
+        movement_time = race_time - hold_time
+        distance_traveled = movement_time * hold_time
+        if distance_traveled > distance_record
+          times_to_winners[race_time] ||= 0
+          times_to_winners[race_time] += 1
+        end
+      end
+    end
+
+    times_to_winners.values.inject(&:*)
   end
 
   def part_2
@@ -15,6 +34,8 @@ class Day06
 
   def _parse_input(input)
     input ||= File.read("config/2023/day_06.txt")
-    # do something with the input
+    input.split(/\n/).map do |line|
+      line.strip.scan(/\d+/).map(&:to_i)
+    end
   end
 end
--- spec/2023/day_06_spec.rb
+++ spec/2023/day_06_spec.rb
@@ -2,21 +2,26 @@ require "pry"
 require_relative "../../lib/2023/day_06.rb"
 
 RSpec.describe Day06 do
+  before do
+    @sample_input = <<~INPUT
+Time:      7  15   30
+Distance:  9  40  200
+INPUT
+  end
+
   context "part 1" do
     it "works for the sample input" do
-      input = ""
-      expect(Day06.new(input).part_1).to eq 0
+      expect(Day06.new(@sample_input).part_1).to eq 288
     end
 
     it "works for the puzzle input" do
-      expect(Day06.new.part_1).to eq 0
+      expect(Day06.new.part_1).to eq 449550
     end
   end
 
   context "part 2" do
     it "works for the sample input" do
-      input = ""
-      expect(Day06.new(input).part_2).to eq 0
+      expect(Day06.new(@sample_input).part_2).to eq 0
     end
 
     it "works for the puzzle input" do
