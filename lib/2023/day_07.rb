a559f8f10a7faa7caa71ec6d4f971b521eaadac1
Aaron Macy <amacy@paypal.com>
2023-12-08 13:03:34 -0500

    day 7, part 1

--- lib/2023/day_07.rb
+++ lib/2023/day_07.rb
@@ -1,10 +1,101 @@
 class Day07
+  Hand = Struct.new(:_cards, :_bid) do
+    CARD_VALUES = {
+      "A" => 14,
+      "K" => 13,
+      "Q" => 12,
+      "J" => 11,
+      "T" => 10,
+      "9" => 9,
+      "8" => 8,
+      "7" => 7,
+      "6" => 6,
+      "5" => 5,
+      "4" => 4,
+      "3" => 3,
+      "2" => 2,
+    }
+
+    def strength
+      case cards.uniq.length
+      when 1
+        7
+      when 2
+        if _count_cards.values.max == 4
+          6 # four of a kind
+        else
+          5 # full house
+        end
+      when 3
+        if _count_cards.values.max == 3
+          4 # three of a kind
+        else
+          3 # two pair
+        end
+      when 4
+        2 # one pair
+      when 5
+        1 # high card
+      else
+        raise "wtf"
+      end
+    end
+
+    def bid
+      _bid.to_i
+    end
+
+    def cards
+      _cards.split(//)
+    end
+
+    def <=>(hand)
+      winner = strength <=> hand.strength
+
+      if _tie?(winner)
+        cards.each_with_index do |card_1, index|
+          card_2 = hand.cards[index]
+          winner = CARD_VALUES[card_1] <=> CARD_VALUES[card_2]
+
+          if _tie?(winner)
+            next
+          else
+            break
+          end
+        end
+      end
+
+      winner
+    end
+
+    private
+
+    def _tie?(int)
+      int == 0
+    end
+
+    def _count_cards
+      cards.inject({}) do |hash, card|
+        hash[card] ||= 0
+        hash[card] += 1
+        hash
+      end
+    end
+  end
+
   def initialize(input=nil)
     @input = _parse_input(input)
   end
 
   def part_1
-    # solve part 1
+    hands = @input
+    total = 0
+
+    hands.sort.each_with_index do |hand, index|
+      total += (hand.bid * (index + 1))
+    end
+
+    total
   end
 
   def part_2
@@ -15,6 +106,10 @@ class Day07
 
   def _parse_input(input)
     input ||= File.read("config/2023/day_07.txt")
-    # do something with the input
+
+    input.split(/\n/).map do |line|
+      hand, bid = line.strip.split(/ /)
+      Hand.new(hand, bid)
+    end
   end
 end
--- spec/2023/day_07_spec.rb
+++ spec/2023/day_07_spec.rb
@@ -2,21 +2,29 @@ require "pry"
 require_relative "../../lib/2023/day_07.rb"
 
 RSpec.describe Day07 do
+  before do
+    @sample_input = <<~INPUT
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+INPUT
+  end
+
   context "part 1" do
     it "works for the sample input" do
-      input = ""
-      expect(Day07.new(input).part_1).to eq 0
+      expect(Day07.new(@sample_input).part_1).to eq 6440
     end
 
     it "works for the puzzle input" do
-      expect(Day07.new.part_1).to eq 0
+      expect(Day07.new.part_1).to eq 250347426
     end
   end
 
   context "part 2" do
     it "works for the sample input" do
-      input = ""
-      expect(Day07.new(input).part_2).to eq 0
+      expect(Day07.new(@sample_input).part_2).to eq 0
     end
 
     it "works for the puzzle input" do
