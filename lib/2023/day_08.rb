7c4a94e91268fa72da2da449f7cbdb06580f3d60
amacy <aaronmacy@gmail.com>
2023-12-08 15:54:53 -0500

    day 8, part 1

--- lib/2023/day_08.rb
+++ lib/2023/day_08.rb
@@ -4,7 +4,18 @@ class Day08
   end
 
   def part_1
-    # solve part 1
+    directions, nodes = @input
+    steps = 0
+
+    current_node = "AAA"
+    loop do
+      directions.each do |direction|
+        index = direction == "L" ? 0 : 1
+        current_node = nodes[current_node][index]
+        steps += 1
+        return steps if current_node == "ZZZ"
+      end
+    end
   end
 
   def part_2
@@ -15,6 +26,14 @@ class Day08
 
   def _parse_input(input)
     input ||= File.read("config/2023/day_08.txt")
-    # do something with the input
+    directions, _, *mappings = input.split(/\n/).map(&:strip)
+
+    nodes = mappings.inject({}) do |hash, line|
+      key, value = line.split(/ = /)
+      hash[key] = value.match(/(\w+), (\w+)/).captures
+      hash
+    end
+
+    [directions.split(//), nodes]
   end
 end
--- spec/2023/day_08_spec.rb
+++ spec/2023/day_08_spec.rb
@@ -4,12 +4,31 @@ require_relative "../../lib/2023/day_08.rb"
 RSpec.describe Day08 do
   context "part 1" do
     it "works for the sample input" do
-      input = ""
-      expect(Day08.new(input).part_1).to eq 0
+      input_1 = <<~INPUT
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+INPUT
+      expect(Day08.new(input_1).part_1).to eq 2
+
+      input_2 = <<~INPUT
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+INPUT
+      expect(Day08.new(input_2).part_1).to eq 6
     end
 
     it "works for the puzzle input" do
-      expect(Day08.new.part_1).to eq 0
+      expect(Day08.new.part_1).to eq 14681
     end
   end
 
