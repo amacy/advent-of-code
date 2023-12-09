d229758c528e27d4113f9c6a0b36df7df21d4475
amacy <aaronmacy@gmail.com>
2023-12-09 10:34:33 -0500

    day 9, part 1 - additional test cases

--- lib/2023/day_09.rb
+++ lib/2023/day_09.rb
@@ -4,7 +4,40 @@ class Day09
   end
 
   def part_1
-    # solve part 1
+    histories = @input
+
+    results = []
+    histories.each_with_index do |history, history_index|
+      results[history_index] = [history]
+      loop do
+        results[history_index] << []
+
+        values = results[history_index][-2]
+        values.each_with_index do |value, value_index|
+          next if value_index == 0
+          results[history_index].last << value - values[value_index - 1]
+        end
+
+        if results[history_index].last.uniq == [0]
+          break
+        end
+      end
+    end
+
+    results.each do |history|
+      reversed = history.reverse
+      reversed.each_with_index do |history_line, row_index|
+        if history_line[-1] == 0
+          history_line << 0
+        else
+          history_line << history_line[-1] + reversed[row_index - 1][-1]
+        end
+      end
+    end
+
+    results.inject(0) do |sum, history|
+      sum += history[0][-1]
+    end
   end
 
   def part_2
@@ -15,6 +48,8 @@ class Day09
 
   def _parse_input(input)
     input ||= File.read("config/2023/day_09.txt")
-    # do something with the input
+    input.split(/\n/).map do |line|
+      line.split(/ /).map(&:to_i)
+    end
   end
 end
--- spec/2023/day_09_spec.rb
+++ spec/2023/day_09_spec.rb
@@ -4,11 +4,26 @@ require_relative "../../lib/2023/day_09.rb"
 RSpec.describe Day09 do
   context "part 1" do
     it "works for the sample input" do
-      input = ""
-      expect(Day09.new(input).part_1).to eq 0
+      input = <<~INPUT
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+INPUT
+      expect(Day09.new(input).part_1).to eq 114
+    end
+
+    it "works for input begins negative and goes positive" do
+      input = "-3 -1 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37"
+      expect(Day09.new(input).part_1).to eq 39
+    end
+
+    it "works for input begins positive and goes negative" do
+      input = "9 6 3 0 -3 -6 -9 -12 -15 -18 -21 -24 -27 -30 -33 -36 -39 -42 -45 -48 -51"
+      expect(Day09.new(input).part_1).to eq -54
     end
 
     it "works for the puzzle input" do
+      # < 1930746034
       expect(Day09.new.part_1).to eq 0
     end
   end
